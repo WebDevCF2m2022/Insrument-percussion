579164863f22b75f86169b797274a82305e0be9a
valerie2107 <kinkamyl@hotmail.com>
2023-04-10 22:18:36 +0200

    update

--- view/public_view/include/search.inc.php
+++ view/public_view/include/search.inc.php
@@ -1,5 +1,5 @@
 <?php
-// Connexion à la base de données
+/*// Connexion à la base de données
 $servername = "localhost";
 $username = "nom_utilisateur";
 $password = "mot_de_passe";
@@ -10,22 +10,29 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Vérifiez la connexion
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+}*/
+$db = new PDO(
+    DB_TYPE.':host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';charset='.DB_CHARSET,
+    DB_USER,
+    DB_PWD
+);
 
+
+$conn=$db;
 // Récupérez la requête de recherche à partir du formulaire soumis
-$search_query = $_POST['search'];
+$search_query = $_GET['search'];
 
 // Interrogez les tables SQL pour récupérer les résultats qui correspondent à la requête de recherche
-$sql = "SELECT * FROM table1 WHERE column1 LIKE '%$search_query%'
-        UNION SELECT * FROM table2 WHERE column2 LIKE '%$search_query%'
-        UNION SELECT * FROM table3 WHERE column3 LIKE '%$search_query%'";
+$sql = "SELECT * FROM categorie WHERE nom_categorie LIKE '%$search_query%'
+        UNION SELECT * FROM instruments WHERE nom LIKE '%$search_query%'
+        UNION SELECT * FROM musiciens WHERE nom_musicien LIKE '%$search_query%'";
 
 $result = $conn->query($sql);
 
-// Afficher les résultats dans une vue conviviale pour les utilisateurs
+// Afficher les résultats pour les users
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo "Résultat: " . $row["column1"] . " - " . $row["column2"] . " - " . $row["column3"] . "<br>";
+    echo "Résultat: " . $row["nom_categorie"] . " - " . $row["nom"] . " - " . $row["nom_musicien"] . "<br>";
   }
 } else {
   echo "Aucun résultat trouvé pour la recherche: " . $search_query;
