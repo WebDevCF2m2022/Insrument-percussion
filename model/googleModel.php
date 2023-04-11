ea47db16b33d4af51351c0cbf926289071a4d9cc
valerie2107 <kinkamyl@hotmail.com>
2023-04-11 15:04:48 +0200

    bla

--- model/googleModel.php
+++ model/googleModel.php
@@ -1,7 +1,7 @@
 <?php
 
 // Récupérez la requête de recherche à partir du formulaire soumis
-$search_query = $_GET['search'];
+$search_query = $_POST['search'];
 
 // Interrogez les tables SQL pour récupérer les résultats qui correspondent à la requête de recherche
 $sql = "SELECT * FROM categorie WHERE nom_categorie LIKE '%$search_query%'
@@ -9,3 +9,12 @@ $sql = "SELECT * FROM categorie WHERE nom_categorie LIKE '%$search_query%'
         UNION SELECT * FROM musiciens WHERE nom_musicien LIKE '%$search_query%'";
 
 $result = $db->query($sql);
+
+// Afficher les résultats pour les users
+if ($result->rowCount() > 0) {
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          echo "Résultat: " . $row["nom_categorie"] . " - " . $row["nom"] . " - " . $row["nom_musicien"] . "<br>";
+        }
+      } else {
+        echo "Aucun résultat trouvé pour la recherche: " . $search_query;
+      }
--- view/public_view/include/search.inc.php
+++ view/public_view/include/search.inc.php
@@ -1,14 +1,5 @@
 <?php
-
-// Afficher les résultats pour les users
-if ($result->rowCount() > 0) {
-  while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "Résultat: " . $row["nom_categorie"] . " - " . $row["nom"] . " - " . $row["nom_musicien"] . "<br>";
-  }
-} else {
-  echo "Aucun résultat trouvé pour la recherche: " . $search_query;
-}
-
+//include_once '../model/googleModel.php';
 ?>
 
 
