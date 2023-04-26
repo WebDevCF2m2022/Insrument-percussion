ed71803be5e2f13b94b84acf3f40a590c364e89a
Slashyo <tom.bik@hotmail.be>
2023-04-26 11:55:18 +0200

    Lire la suite des catégories, ok!

--- view/public_view/electrophone.php
+++ view/public_view/electrophone.php
@@ -10,7 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
-    <script defer  src="js/captcha.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script defer  src="js/lireLaSuite.js"></script>
 
     <title>Document</title>
 </head>
@@ -33,18 +34,26 @@ foreach ($elec as $key => $value):
 
 ?>
     <div class="fiche">
-        <div class="imgWrapper">
-            <img class="imgFiche" src="<?=$value['img_url']?>" alt="<?=$value['nom']?>" width="150px">
-        </div>
-        <div class="contentWrapper">
-            <h1 class="nomFiche"><?=$value['nom'];?></h1>
-            <h5 class="resumeFiche"><?=$value['resume'];?></h5>
-        </div>
-        <div class="moreInfo">
-            <a href="#">Lire la suite...</a>
-            
+    <div class="imgWrapper">
+        <img class="imgFiche" src="<?=$value['img_url']?>" alt="<?=$value['nom']?>" width="150px">
+    </div>
+    <div class="contentWrapper">
+        <h1 class="nomFiche"><?=$value['nom'];?></h1>
+        <h5 class="resumeFiche"><?=$value['resume'];?></h5>
+        <div class="hiddenInfo" style="display:none;">
+            <p>Description: <?=$value['description'];?></p>
+            <p><a href="<?=$value['url'];?>" target="_blank">Lien vers Wikipédia</a></p>
+            <audio controls>
+                <source src="<?=$value['son'];?>" type="audio/mp3">
+                Votre navigateur ne supporte pas l'élément audio.
+            </audio>
+            <button class="fermerInfo">Fermer</button>
         </div>
     </div>
+    <div class="moreInfo">
+        <a href="#" class="plusInfo">Lire la suite...</a>
+    </div>
+</div>
 
 <?php endforeach; ?>
 </div>
