c02a1012fc1dc34c5121f6a8589544ad2ddf5e06
Jaouad1060 <jaouad1060@hotmail.com>
2023-05-15 09:33:44 +0200

    jaouadfin

--- view/private_view/adminView.php
+++ view/private_view/adminView.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Admin</title>
 </head>
 <body>
@@ -17,17 +18,26 @@
          } 
          th, td { 
             padding: 5px; } 
+
+            h1{
+                text-align: center;
+            }
+            a{
+            text-decoration: none;
+            padding: 5px;
+            }
             </style>
             <table>
             <thead> 
                 <tr> 
                     <th>ID</th>
-                     <th>Resume</th> 
+                     <th>Resumer</th> 
                      <th>Description</th> 
                      <th>Son</th> 
                      <th>Url</th> 
-                     <th>Update</th> 
-                     <th>Delete</th> 
+                     <th>Numero</th> 
+                     <th>Modifier</th> 
+                     <th>Supprimer</th> 
                     </tr>
              </thead><tbody>
     
